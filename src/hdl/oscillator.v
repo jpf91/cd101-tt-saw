882be223b14ad010e4f0f5b2f2a4ecee9eee7972
Johannes Pfau <johannes.pfau@kit.edu>
2025-02-20 15:45:48 +0000

    Replace rectangle with saw wave

--- src/hdl/oscillator.v
+++ src/hdl/oscillator.v
@@ -6,31 +6,20 @@ module oscillator(
     input clk,
     input rstn,
     input[11:0] count_max,
-    output reg[7:0] data
+    output[7:0] data
 );
 
     reg[11:0] counter;
     reg data_buf;
 
     always @(posedge clk) begin
-        counter <= counter + 1;
         if (rstn == 0) begin
             counter <= 0;
-            data_buf <= 0;
-        end
-        if (counter == count_max) begin
-            counter <= 0;
-            data_buf <= !data_buf;
-        end
-    end
-
-
-    always @(*) begin
-        if (data_buf == 0) begin
-            data = 0;
         end else begin
-            data = 8'hFF;
+            counter <= counter + count_max;
         end
     end
+
+    assign data = counter[11:4];
 
 endmodule
